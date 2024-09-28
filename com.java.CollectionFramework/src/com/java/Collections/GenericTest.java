bd385cda62ac2dd2c6f86396a3d2a1dae7c85d6b
e5433521 <e5433521@192.168.1.77>
2024-09-28 16:37:11 +0530

    Collection Framework Practice

---
+++ com.java.CollectionFramework/src/com/java/Collections/GenericTest.java
@@ -0,0 +1,20 @@
+package com.java.Collections;
+
+import java.util.ArrayList;
+
+class Allen
+{
+	
+}
+
+public class GenericTest {
+
+	public static void main(String[] args) {
+		
+		ArrayList<? extends Allen> a= new ArrayList<>();
+		
+		ArrayList<Object> agd= new ArrayList<>();
+				
+	}
+
+}
